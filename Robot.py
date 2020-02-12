cbec936ba191605c831b5667d08edc8b76869059
Gabriel Entov <gentov97@gmail.com>
2020-02-11 20:27:22 -0500

    Battery dead, slowed down PWM for next test

--- Robot.py
+++ Robot.py
@@ -34,7 +34,7 @@ class Car():
         currentAngle = self.voltageToAngle(self.adc.AnalogRead(0).voltage)
         newAngle = currentAngle
         while(newAngle > (currentAngle - degrees)):
-            self.steeringMotor.turn(-90)
+            self.steeringMotor.turn(-2)
             newAngle = self.voltageToAngle(self.adc.AnalogRead(0).voltage)
         self.steeringMotor.turn(0)
         print("Turned Right")
@@ -45,7 +45,7 @@ class Car():
         currentAngle = self.voltageToAngle(self.adc.AnalogRead(0).voltage)
         newAngle = currentAngle
         while (newAngle < (degrees + currentAngle)):
-            self.steeringMotor.turn(90)
+            self.steeringMotor.turn(2)
             newAngle = self.voltageToAngle(self.adc.AnalogRead(0).voltage)
         self.steeringMotor.turn(0)
         print("Turned Left")
