dfb28bb8cd624d36f57f982982fa03dda9500a41
Nick <Nickjl0809@gmail.com>
2020-03-23 12:59:00 -0400

    just looked through code

--- Robot.py
+++ Robot.py
@@ -17,10 +17,10 @@ class Car():
         self.steeringMotor = Motor(carParams.steeringMotorDirPin.value, carParams.steeringMotorPWMPin.value)
         #put an encoder on this one
         self.drivingMotor = Motor(carParams.driveMotorDirPin.value, carParams.driveMotorPWMPin.value, encoderA=21, encoderB=20)
-        pass
+
 
     def driveForward(self, distance):
-	    pass
+        pass
 
     def voltageToAngle(self, voltage):
         #takes voltage value and turns it into an angle
@@ -63,11 +63,13 @@ class Car():
 
         print("Went to angle:", self.voltageToAngle(self.adc.AnalogRead(0).voltage))
 
-c = Car()
-#while(1):
-    #print(c.voltageToAngle(c.adc.AnalogRead(0).voltage))
-# c.turnRightRelative(10)
-# time.sleep(1.5)
-# c.turnLeftRelative(45)
-# c.turnRightRelative(45)
-c.turnAbsolute(0)
+if __name__ == '__main__':
+    c = Car()
+    c.drivingMotor.turn(100)
+    #while(1):
+        #print(c.voltageToAngle(c.adc.AnalogRead(0).voltage))
+    # c.turnRightRelative(10)
+    # time.sleep(1.5)
+    # c.turnLeftRelative(45)
+    # c.turnRightRelative(45)
+    #c.turnAbsolute(0)
